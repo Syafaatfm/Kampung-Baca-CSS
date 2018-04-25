0c6255852aa2fa35ae3d4686137bfc7902c12d32
Syafaatfm <sial.gua@gmail.com>
2018-04-25 07:18:26 +0700

    Adsense Code Body

---
+++ adsense-code.js
@@ -0,0 +1,8 @@
+<b:if cond='data:blog.pageType not in {&quot;static_page&quot;,&quot;error_page&quot;} and data:blog.isMobileRequest == &quot;true&quot; and not data:blog.searchQuery'>
+<div class='sticky-ad' id='sticky-ad'>
+<!-- Kode iklan silahkan simpan di bawah ini -->
+<!-- 320x50 -->
+
+<button aria-label='Close this ad' class='sticky-ad-close-button' onclick='document.getElementById(&apos;sticky-ad&apos;).style.display=&apos;none&apos;;'/>
+</div>
+</b:if>
